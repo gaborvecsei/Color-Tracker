5310586f943fea6a9280f84d70da91b4dd9fba45
Gábor Vecsei <vecseigabor.x@gmail.com>
2020-10-22 20:16:37 +0200

    Publish package to PyPI when release is created on Github (#21)
    * github action for upload to pypi when released
    
    * blog and website link change in comments
    
    * version fixed in __init__.py

--- color_tracker/__init__.py
+++ color_tracker/__init__.py
@@ -3,8 +3,8 @@
 *               Color Tracker
 *
 *              Gabor Vecsei
-* Website:     gaborvecsei.com
-* Blog:        https://gaborvecsei.wordpress.com/
+* Website:     https://gaborvecsei.com
+* Blog:        https://gaborvecsei.com
 * LinkedIn:    https://www.linkedin.com/in/gaborvecsei
 * Github:      https://github.com/gaborvecsei
 *
@@ -16,4 +16,4 @@ from .utils import HSVColorRangeDetector
 from .utils.camera import WebCamera
 
 __author__ = "Gabor Vecsei"
-__version__ = "0.0.4"
+__version__ = "0.1.1"
